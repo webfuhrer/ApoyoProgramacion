f645fcf935f1784810e4b131865bca97a83c4d4d
Luis Sanz Goizueta <ataraxa@hotmail.com>
2023-05-16 10:25:21 +0200

    Add files via upload

---
+++ ListaDoble/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDoble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaDoble lista_doble=new ListaDoble();
+            lista_doble.insertarPrincipio(16);
+            lista_doble.insertarPrincipio(5);
+            lista_doble.insertarPrincipio(2023);
+            Console.ReadLine();
+        }
+    }
+    internal class ListaDoble
+    {
+        Nodo cabeza;
+        Nodo cola;
+
+        public ListaDoble()
+        {
+            this.cabeza = null;
+            this.cola = null;
+        }
+        public void insertarPrincipio(int valor)
+        {
+            //Creo el nuevo nodo:
+            Nodo nuevo_nodo = new Nodo(valor);
+            if (cabeza == null)
+            {
+                cabeza = nuevo_nodo;
+                cola= nuevo_nodo;
+            }
+            else
+            {
+                //Como quiero insertar por el principìo
+                //La nueva cabeza será mi nuevo_nodo
+                //Y el siguiente de mi nuevo nodo,
+                //será la antigua cabeza
+                Nodo antigua_cabeza = cabeza;
+                //La cabeza será ahora mi nuevo_nodo:
+                cabeza = nuevo_nodo;
+                //El siguiente de mi nuevo_nodo, seá la antigua cabeza:
+                nuevo_nodo.Siguiente = antigua_cabeza;
+                //El anterior de la antigua cabeza, será el nuevo nodo
+                antigua_cabeza.Anterior = nuevo_nodo;
+            }
+        }
+    }
+    internal class Nodo
+    {
+        Nodo siguiente, anterior;
+        int valor;
+
+        public Nodo( int valor)
+        {
+            this.Siguiente = null;
+            this.Anterior = null;
+            this.valor = valor;
+        }
+
+        internal Nodo Siguiente { get => siguiente; set => siguiente = value; }
+        internal Nodo Anterior { get => anterior; set => anterior = value; }
+    }
+}
---
+++ Pila/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pila p= new Pila();
+            p.Desapilar();
+            p.Apilar(16);
+            p.Apilar(5);
+            p.Apilar(2023);
+            Console.WriteLine("ANTES DE DESAPILAR");
+            p.Imprimir();
+            p.Desapilar();
+            Console.WriteLine("DESPUÉS DE DESAPILAR");
+            p.Imprimir();
+
+            Console.ReadLine();
+        }
+    }
+    internal class Pila
+    {
+        Nodo cabeza;
+
+        public Pila()
+        {
+            this.cabeza = null;
+        }
+        public void Apilar(int valor)
+        {
+            //1-Creo un nuevo nodo
+            Nodo nuevo_nodo = new Nodo(valor);
+            //Si la pila está vacía, la cabeza debe apuntar
+            //al nuevo nodo
+
+            if (cabeza == null)
+            {
+                cabeza = nuevo_nodo;
+            }
+            else
+            {
+                
+                //Meto en una variable la antigua cabeza para
+                //no perder la referencia, porque si la pierdo, mis pilas 
+                //solo tendrán un elemento
+                Nodo antigua_cabeza=cabeza;
+                //Apunto la cabeza al nuevo nodo
+                cabeza = nuevo_nodo;
+                //El nuevo nodo apunta a la antigua cabeza
+                nuevo_nodo.Siguiente = antigua_cabeza;
+            }
+        }
+        public void Desapilar()
+        {
+            //Desapilar consiste en hacer que la cabeza sea el siguiente 
+            //del actual
+            if (cabeza==null)
+            {
+                Console.WriteLine("La pila está vacía");
+            }
+            else
+            {
+                Console.WriteLine("Desapilando el "+cabeza.Valor);
+                cabeza = cabeza.Siguiente;
+
+            }
+        }
+        public void Imprimir()
+        {
+            Nodo nodo = cabeza;
+            while(nodo!=null)
+            {
+                int valor = nodo.Valor;
+                Console.WriteLine("El valor es: " + valor);
+                nodo = nodo.Siguiente;
+            }
+        }
+    }
+    internal class Nodo
+    {
+        int valor;
+        Nodo siguiente;
+
+        public Nodo(int valor)
+        {
+            this.valor = valor;
+            this.siguiente = null;
+        }
+
+        public int Valor { get => valor; set => valor = value; }
+        internal Nodo Siguiente { get => siguiente; set => siguiente = value; }
+    }
+}
